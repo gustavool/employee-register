0e2e86cee570d52dd5e69ba2961e213082facc8a
Gustavo de Oliveira <gustavool@live.com>
2021-09-17 21:18:22 -0300

    env variables added

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'postgres',
+  port: process.env.TYPEORM_PORT,
+  host: process.env.TYPEORM_HOST,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  migrations: ['./src/database/migrations/*.ts'],
+  entities: ['./src/entities/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+};
