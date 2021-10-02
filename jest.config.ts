3d52cf87a19e08ff61ce9993b40103191ef9918a
Gustavo de Oliveira <gustavool@live.com>
2021-10-02 20:32:13 -0300

    add coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,9 @@
 export default {
   clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/services/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text-summary', 'lcov'],
   coverageProvider: 'v8',
   preset: 'ts-jest',
   testMatch: ['**/*.spec.ts'],
